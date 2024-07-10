eb09f1fd5435010bd8f7ac165b4ffec9d71bb7e5
tomlausmarthon <tom.lau.smarthon@gmail.com>
2024-07-10 17:58:00 +0800

    Revert "add IR sensor extension"
    This reverts commit 96d363e71e4a18e56b1f64e9f078a6eb468b6330.

--- sensor.ts
+++ sensor.ts
@@ -1109,13 +1109,11 @@ export enum TextOption {
   export function isLcdConnected(): boolean {
     return !!lcdState || connect();
   }
-    //% blockId="smarthon_IR_detector"
-    //% block="Object detected at %pin"
-
-    export function IRdetection(pin: DigitalPin): boolean {
-        if (pins.digitalReadPin(pin) == 0)
-            return false;
-        else return true;
-    }
+  
+  export function IRdetection(pin: DigitalPin): boolean {
+    if (pins.digitalReadPin(pin) == 0)
+        return false;
+    else return true;
+}
 
 }
