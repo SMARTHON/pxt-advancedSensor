ea6fe5417eb3334c980a2e224e6a58634ff407db
tomlausmarthon <tom.lau.smarthon@gmail.com>
2024-07-16 14:43:40 +0800

    Added a new tab Environment and put the block of battery level into it

--- sensor.ts
+++ sensor.ts
@@ -1111,4 +1111,41 @@ export enum TextOption {
   }
 
 
+	//Sunlight Charging Module
+	//------------------------------
+	let battery_level = 0
+
+	//% subcategory=Environment
+	//% weight=70
+	//% group="Sunlight Charging"
+	//% blockId="smarthon_get_battery_level"
+	//% block="Get battery level (percentage) at Pin %pin"
+  
+	export function getBattery(pin: AnalogPin): number {
+		let max = 660;   // define the max reading from battery
+		let min = 560;   // define the min reading from battery
+		let sum = 0;
+
+		//  read 30 times to get the average
+		for (let n = 0; n < 30; n++) {
+			sum += pins.analogReadPin(pin);
+			basic.pause(10);
+		}
+		let avg = sum / 30;
+		OLED.writeNumNewLine(avg);
+
+		if (avg > min && avg < max) {
+			battery_level = Math.ceil(
+				pins.map(avg, min, max, 0, 100) / 5) * 5;
+			//  round up to multiple of 5
+		}
+		else if (avg > max) {
+			battery_level = 100;
+		}
+		else {
+			battery_level = 0;
+		}
+		return battery_level;
+	}
+
 }
