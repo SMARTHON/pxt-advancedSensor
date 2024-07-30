110161654c9beb18b9ff2d76c5edae523502cfd5
tomlausmarthon <tom.lau.smarthon@gmail.com>
2024-07-30 12:50:21 +0800

    Added a branched "PH Sensor" and added 2 blocks into it, removed 2 blocks

--- sensor.ts
+++ sensor.ts
@@ -50,52 +50,7 @@ namespace AdvancedModule {
         return 85;
     }
 
-    let ph_value = ""
-    //% blockId="readPH"
-    //% block="Read PH value at %pin"
-    //% weight=80 group="Water"
-	//% subcategory="Science"
-    export function readPH(pin: AnalogPin): string {
-        let sensorarray: number[] = []
-        for (let i = 0; i < 10; i++) {
-            sensorarray.push(pins.analogReadPin(AnalogPin.P0))
-            basic.pause(10)
-        }
-        sensorarray.sort((n1, n2) => n1 - n2);
-        for (let value of sensorarray) {
-            serial.writeLine(value.toString())
-        }
-        ph_value = (sensorarray[5] * 5 * 10 * 35 / 1024).toString()
-        serial.writeLine("===========")
-        if (ph_value.length == 3) {
-            serial.writeLine("PH: " + ph_value.substr(0, 1) + "." + ph_value.substr(1, ph_value.length))
-            return ph_value.substr(0, 1) + "." + ph_value.substr(1, ph_value.length)
-        } else {
-            serial.writeLine("PH: " + ph_value.substr(0, 2) + "." + ph_value.substr(2, ph_value.length))
-            return ph_value.substr(0, 2) + "." + ph_value.substr(2, ph_value.length)
-        }
-    }
-    let ph_value_number = 0
-    //% blockId="readPHNumber"
-    //% block="Read PH value (x100) pin %ports| offset %offset"
-    //% weight=70 group="Water"
-	//% subcategory="Science"
-    export function readPhNumber(ports: AnalogPin, offset: number): number {
 
-        let temp = 0;
-        temp = ports
-        let sensorarray: number[] = []
-        for (let i = 0; i < 10; i++) {
-            sensorarray.push(pins.analogReadPin(temp))
-            basic.pause(10)
-        }
-        sensorarray.sort((n1, n2) => n1 - n2);
-        for (let value of sensorarray) {
-            serial.writeLine(value.toString())
-        }
-        ph_value_number = (sensorarray[5] * 5 * 10 * 35 / 1024) + offset
-        return ph_value_number
-    }
     /**
     * get water level value (0~100)
     * @param waterlevelpin describe parameter here, eg: AnalogPin.P0
@@ -1290,6 +1245,46 @@ export enum TextOption {
   export function isLcdConnected(): boolean {
     return !!lcdState || connect();
   }
+
+//PH Sensor
+//--------------------------------------------
+    let ph_value_number = 0
+    //% blockId="readPHNumber"
+    //% block="Read PH value pin %ports| offset %offset"
+    //% weight=70 group="PH Sensor"
+	//% subcategory=Science
+    export function readPhNumber(ports: AnalogPin, offset: number): number {
+
+        let temp = 0;
+        temp = ports
+        let sensorarray: number[] = []
+        let avgValue: number = 0;
+        for (let i = 0; i < 10; i++) {
+            sensorarray.push(pins.analogReadPin(temp))
+            basic.pause(10)
+        }
+        sensorarray.sort((n1, n2) => n1 - n2);
+        for (let i = 2; i < 8; i++) {
+            avgValue += sensorarray[i];
+        }
+
+        avgValue = avgValue / 6
+        ph_value_number = ((avgValue * 5 / 1024 )*8.1677 + offset -17.561)
+        return ph_value_number 
+    }
+    let Voltage =0
+    let pH_Value =0
+    //% blockId="readVoltage"
+    //% block="Read Voltage value pin %ports"
+    //% weight=70 group="PH Sensor"
+	//% subcategory=Science
+    export function ShowVoltage(ports: AnalogPin): number {
+
+        pH_Value = pins.analogReadPin(ports);
+        Voltage = pH_Value * (5.0 / 1023.0);
+        return Voltage
+    }
+
 
 
 }
